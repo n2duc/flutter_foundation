3ff9a4dbe56137c98199450e04b80db1fe8387be
n2duc <ngocduc8122002@gmail.com>
2025-09-08 18:04:55 +0700

    update: payment page`

--- flutter_base/lib/cart/ui/view/payment_page.dart
+++ flutter_base/lib/cart/ui/view/payment_page.dart
@@ -51,38 +51,345 @@ class PaymentPage extends StatelessWidget {
                 borderRadius: BorderRadius.circular(RFXSpacing.spacing12),
               ),
               padding: const EdgeInsets.all(RFXSpacing.spacing12),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+              child: Column(
                 children: [
-                  Icon(
-                    Iconsax.location,
-                    size: RFXSpacing.spacing18,
-                    color: Colors.deepOrangeAccent,
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Iconsax.location,
+                        size: RFXSpacing.spacing18,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      const SizedBox(width: RFXSpacing.spacing8),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Nguyen Ngoc Duc',
+                              style: textTheme.bodyMedium?.copyWith(
+                                fontWeight: FontWeight.w600,
+                                color: RFXColors.lightPrimary,
+                              ),
+                            ),
+                            const SizedBox(height: RFXSpacing.spacing2),
+                            Text('+84 123 456 789', style: textTheme.bodySmall),
+                            const SizedBox(height: RFXSpacing.spacing2),
+                            Text(
+                              '123, ABC Street, City, Country',
+                              style: textTheme.bodySmall,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Icon(
+                        Iconsax.arrow_right_3_copy,
+                        size: RFXSpacing.spacing16,
+                      ),
+                    ],
                   ),
-                  const SizedBox(width: RFXSpacing.spacing8),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  const SizedBox(height: RFXSpacing.spacing12),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Iconsax.wallet_copy,
+                            size: RFXSpacing.spacing16,
+                            color: RFXColors.lightPrimary,
+                          ),
+                          const SizedBox(width: RFXSpacing.spacing8),
+                          Text(
+                            'Identity information',
+                            style: textTheme.bodySmall,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text('*****1234', style: textTheme.bodySmall),
+                          const SizedBox(width: RFXSpacing.spacing6),
+                          Icon(
+                            Iconsax.arrow_right_3_copy,
+                            size: RFXSpacing.spacing16,
+                            color: Colors.grey.shade600,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: RFXSpacing.spacing12),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(RFXSpacing.spacing12),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      right: RFXSpacing.spacing12,
+                      left: RFXSpacing.spacing12,
+                      top: RFXSpacing.spacing12,
+                    ),
+                    child: Row(
                       children: [
+                        Icon(
+                          Iconsax.shop_copy,
+                          size: RFXSpacing.spacing16,
+                          color: RFXColors.lightPrimary,
+                        ),
+                        const SizedBox(width: RFXSpacing.spacing8),
                         Text(
-                          'Nguyen Ngoc Duc',
+                          'Order info',
                           style: textTheme.bodyMedium?.copyWith(
                             fontWeight: FontWeight.w600,
                             color: RFXColors.lightPrimary,
                           ),
                         ),
-                        const SizedBox(height: RFXSpacing.spacing2),
-                        Text('+84 123 456 789', style: textTheme.bodySmall),
-                        const SizedBox(height: RFXSpacing.spacing2),
-                        Text(
-                          '123, ABC Street, City, Country',
-                          style: textTheme.bodySmall,
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: RFXSpacing.spacing12,
+                      vertical: RFXSpacing.spacing12,
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey, width: 0.5),
+                            borderRadius: BorderRadius.circular(
+                              RFXSpacing.spacing8,
+                            ),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(
+                              RFXSpacing.spacing6,
+                            ),
+                            child: Image.asset(
+                              'assets/images/hanoi.jpg',
+                              width: RFXSpacing.spacing100,
+                              height: RFXSpacing.spacing80,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: RFXSpacing.spacing12),
+                        Expanded(
+                          child: SizedBox(
+                            height: RFXSpacing.spacing80,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  'Hanoi City Tour',
+                                  style: textTheme.bodyMedium?.copyWith(
+                                    fontWeight: FontWeight.w600,
+                                    color: RFXColors.lightPrimary,
+                                  ),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                Text(
+                                  'A beautiful cruise through the limestone islands of Ha Long Bay',
+                                  style: textTheme.bodySmall,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      '\$200.00',
+                                      style: textTheme.bodyMedium?.copyWith(
+                                        fontWeight: FontWeight.w600,
+                                        color: RFXColors.lightPrimary,
+                                      ),
+                                    ),
+                                    Text(' x1', style: textTheme.bodySmall),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                  Icon(Iconsax.arrow_right_3_copy, size: RFXSpacing.spacing18),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        top: BorderSide(
+                          color: Colors.grey.shade300,
+                          width: 0.5,
+                        ),
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: RFXSpacing.spacing8,
+                      horizontal: RFXSpacing.spacing12,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Total (for 1 item)',
+                          style: textTheme.bodySmall?.copyWith(
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          '\$200.00',
+                          style: textTheme.bodyMedium?.copyWith(
+                            fontWeight: FontWeight.w600,
+                            color: RFXColors.lightPrimary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
+              ),
+            ),
+            const SizedBox(height: RFXSpacing.spacing12),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(RFXSpacing.spacing12),
+              ),
+              padding: const EdgeInsets.all(RFXSpacing.spacing12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Payment Method',
+                    style: textTheme.bodyMedium?.copyWith(
+                      fontWeight: FontWeight.w600,
+                      color: RFXColors.lightPrimary,
+                    ),
+                  ),
+                  const SizedBox(height: RFXSpacing.spacing12),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Iconsax.wallet_copy,
+                            size: RFXSpacing.spacing16,
+                            color: RFXColors.lightPrimary,
+                          ),
+                          const SizedBox(width: RFXSpacing.spacing8),
+                          Text('Credit/Debit Card', style: textTheme.bodySmall),
+                        ],
+                      ),
+                      Icon(
+                        Iconsax.arrow_right_3_copy,
+                        size: RFXSpacing.spacing16,
+                        color: Colors.grey.shade600,
+                      ),
+                      const SizedBox(width: RFXSpacing.spacing8),
+                      Text('**** **** **** 1234', style: textTheme.bodySmall),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: RFXSpacing.spacing12),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(RFXSpacing.spacing12),
+              ),
+              padding: const EdgeInsets.all(RFXSpacing.spacing12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Payment Details',
+                    style: textTheme.bodyMedium?.copyWith(
+                      fontWeight: FontWeight.w600,
+                      color: RFXColors.lightPrimary,
+                    ),
+                  ),
+                  const SizedBox(height: RFXSpacing.spacing12),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Text('Subtotal'), Text('\$200.00')],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Text('Booking fee'), Text('\$10.00')],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Discount'),
+                      Text(
+                        '-\$10.00',
+                        style: textTheme.bodyMedium?.copyWith(
+                          color: RFXColors.lightPrimary,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: RFXSpacing.spacing8),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        top: BorderSide(
+                          color: Colors.grey.shade300,
+                          width: 0.5,
+                        ),
+                      ),
+                    ),
+                    padding: const EdgeInsets.only(top: RFXSpacing.spacing8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Total'),
+                        Text(
+                          '\$200.00',
+                          style: textTheme.bodyMedium?.copyWith(
+                            fontWeight: FontWeight.w600,
+                            color: RFXColors.lightPrimary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text.rich(
+                TextSpan(
+                  text:
+                      'By clicking "Order now", you agree to comply with the RFX ',
+                  style: textTheme.bodySmall?.copyWith(
+                    color: Colors.grey.shade800,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Terms and Conditions',
+                      style: textTheme.bodySmall?.copyWith(
+                        color: RFXColors.lightPrimary,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
