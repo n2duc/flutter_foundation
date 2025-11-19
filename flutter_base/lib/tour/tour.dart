19b566b30a45c15475107b62423b570f4355201e
n2duc <ngocduc8122002@gmail.com>
2025-11-19 14:54:45 +0700

    nylo base

--- flutter_base/lib/tour/tour.dart
+++ flutter_base/lib/tour/tour.dart
@@ -2,7 +2,7 @@ export 'data/models/tour_data.dart';
 export 'data/repositories/tour_repositories.dart';
 export 'ui/view/tour_detail_page.dart';
 export 'ui/view/destination_page.dart';
-export 'ui/view/tour_item.dart';
+export 'ui/view/widgets/tour_item.dart';
 export 'ui/view/tour_booking_sheet.dart';
 export 'ui/cubit/tour_cubit.dart';
 export 'ui/view/rating_stars.dart';
